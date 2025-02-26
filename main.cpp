8dce61329f7bea286ff9f531a6fe4e3f7f1289df
Geovani_Dev <davidolvera475@gmail.com>
2025-02-26 17:47:04 -0600

    Clase 26_02_25 - Unidad3: Punteros

--- main.cpp
+++ main.cpp
@@ -2,7 +2,22 @@
 
 using namespace std;
 
+void cambio(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 int main() {
-    cout << "Estructura de datos" << endl;
+    int x = 10, y = 9;
+
+    cout << "Antes de cambiar x: " << x << endl;
+    cout << "Antes de cambiar y: " << y << endl;
+
+    cambio(&x, &y);
+
+    cout << "Cambio de x: " << x << endl;
+    cout << "Cambio de y: " << y << endl;
+
     return 0;
 }
